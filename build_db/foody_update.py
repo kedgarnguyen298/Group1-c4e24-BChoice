539d436f0adc0c4669d2f702d9259ec6ec3fcd61
VanNguyen1102 <vanb1192@gmail.com>
2019-01-26 15:11:30 +0700

    initial commit

--- build_db/foody_update.py
+++
@@ -1,7 +0,0 @@
-import mongoengine
-import mlab
-from models.foody_model import Foody
-
-mlab.connect()
-
-Foody.objects.update(set__title="Cơm")
--- build_db/models/foody_model.py
+++
@@ -1,8 +0,0 @@
-from mongoengine import *
-
-class Foody(Document):
-    title = StringField()
-    name = StringField()
-    address = StringField()
-    image = URLField()
-    rate = IntField()
