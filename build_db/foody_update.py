eff4fa6e25c853f780f125a5722b6daebf85e1c2
kedgarnguyen298 <kedgarnguyen298@gmail.com>
2019-02-14 01:26:40 +0700

    fix loi chi Van

--- build_db/foody_update.py
+++
@@ -1,8 +0,0 @@
-import mongoengine
-import mlab
-from models.foody_model import Foody
-
-if __name__ == "__main__":    
-    mlab.connect()
-
-    Foody.objects.delete()
--- build_db/test.py
+++
@@ -1,3 +0,0 @@
-from decode_test import no_accent_vietnamese
-
-print(no_accent_vietnamese("Việt Nam Đất Nước Con Người").lower())
